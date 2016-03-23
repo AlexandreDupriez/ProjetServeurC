11e755ad0eff143577ba56961ab409493dc21ef3
Azusa <azusa@localhost.localdomain>
2016-03-23 23:54:31 +0100

    chapitre 6 terminé

--- webserver/socket.c
+++ webserver/socket.c
@@ -7,64 +7,13 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-char* concat(char *s1, char *s2)
-{
-    char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the zero-terminator
-    //in real code you would check for errors in malloc here
-    strcpy(result, s1);
-    strcat(result, s2);
-    return result;
-}
+#include "socket.h"
 
-int analyse_entete(const char * message){
-	
-	int nb_mots=0;
-	unsigned int i = 0;
-	char * delim;
-	char * buf;
-	int err404 = 0;
-
-	buf= strdup(message);
-	delim = strtok(buf," ");
-
-	if(strncmp(delim, "GET", 4) != 0)
-		return -1;
-
-	while(i<strlen(message)){
-		if(message[i]== ' ')
-			nb_mots++;
-		i++;
-    }
-		if(nb_mots!=2)
-			return -1;
-
-		delim = strtok(NULL," ");
-		if(strcmp("/",delim)!=0)
-			err404=1;
-
-		delim = strtok(NULL," ");
-	if((strcmp(delim, "HTTP/1.0\n") != 0) && (strcmp(delim, "HTTP/1.1\n") != 0))	
-		return -1;
-
-	if(err404)
-		return 404;
-
-	return 0;
-
-}
-
-void envoie_reponse(FILE * fclient, const char * phrase) {
-	fprintf(fclient, "<Alakazam> %s\n", phrase);
-}
 
 char * fgets_or_exit(char * buffer , int size , FILE * stream) {
 
-
-    if(fgets(buffer, size, stream) == NULL)
-    	exit(EXIT_FAILURE);
-	
-	fflush(stdout);
-
+	if(fgets(buffer, size, stream) == NULL)
+		exit(1);
 	return buffer;
 }
 
@@ -96,7 +45,93 @@ void initialiser_signaux(void){
 
 }
 
+int analyse_GET(const char * buf, http_request * request) {
+	request->method = HTTP_UNSUPPORTED;	
+	if(strncmp(buf, "GET ", 4) != 0)
+		return -1;
+	request->method = HTTP_GET;
+	return 0;
+}
+
+int trois_mots(const char * buf, http_request * request) {
+	
+	char * delim;
+	char * buffer;
+    buffer= strdup(buf);
+	if((delim = strtok(buffer," "))==NULL)
+		return -1;
+
+    if((delim = strtok(NULL," "))==NULL){	
+	return -1;
+	}
+	request->url = delim;
+	printf("%s\n", request->url);
+
+
+
+	if((delim = strtok(NULL," "))==NULL)
+		return -1;
+
+
+	return 0;
+}
+
+int analyse_HTTP(const char * buf, http_request * request) {
+
+	char * delim;
+	char * buffer;
+    buffer= strdup(buf);
+	delim = strtok(buffer," ");
+	delim = strtok(NULL," ");
+	delim = strtok(NULL," ");
+
+	if(strcmp(delim, "HTTP/1.1\n") == 0 || strcmp(delim, "HTTP/1.0\n") == 0) {
+
+		if(strcmp(delim, "HTTP/1.0\n") == 0){
+			request->major_version = 1;
+			request->minor_version = 0;
+		}else if(strcmp(delim, "HTTP/1.1\n") == 0){
+			request->major_version = 1;
+			request->minor_version = 1;
+		}
+
+		return 0;
+	}
+	return -1;
+}
+
+
+int parse_http_request(const char *request_line , http_request *request)
+{
+	analyse_GET(request_line, request);
+ 	if(trois_mots(request_line, request)!=0 || analyse_HTTP(request_line, request)!=0)
+		return -1;
+	return 0;
+}
+
+void skip_headers(FILE *client)
+{
+    char line[255];
+	
+	while(fgets_or_exit(line, 255, client) != NULL && strcmp(line, "\n") != 0 && strcmp(line, "\r\n") != 0);
+}
+
+void send_status(FILE *client , int code , const char *reason_phrase)
+{
+  fprintf(client, "HTTP/1.1 %d %s\n", code, reason_phrase);
+}
+
+void send_response(FILE *client , int code , const  char *reason_phrase , const  char *message_body)
+{
+  send_status(client, code, reason_phrase);
+  fprintf(client, "Content-Length : %d\r\n\r\n", (int)strlen(message_body));
+  fprintf(client, message_body);
+  fflush(client);  
+}
+
 int creer_serveur(int port){
+
+	initialiser_signaux();
 
 	int socket_serveur;
 	socket_serveur = socket (AF_INET,SOCK_STREAM,0);
@@ -132,70 +167,50 @@ int creer_serveur(int port){
 	while(1){
 
 		int socket_client ;
-		FILE * fclient;
-		initialiser_signaux();
+	
+		
+		
 		socket_client = accept(socket_serveur, NULL, NULL );
 		if(socket_client == -1)
 		{
 			perror ("accept");
-			/* traitement d’erreur */
+			return 1;
+			//traitement d’erreur
 		}
+
+
 	//On peut maintenant dialoguer avec le client
 
-	char * message_bienvenue="\nBienvenue sur le serveur de Alexandre Dupriez et Bastien Dusart. Pour vous accueillir dans les meilleures conditions, nous vous offrons ce sublime poème:\n\nMa souris.\nMa souris s'est pendue ce matin.\nJe me doutais bien que quelque chose\nNe tournait pas rond. La mollette sans entrain,\nLe pointeur sans vigueur, tout semblait morose.\n\nEn allumant mon écran j'ai vite reconnu\nSon écriture en Gothic (12pts) en gras.\nC'était son testament, sa lettre de pendue\nQui, le clavier à se consoler, aidera.\n\nJe savais que la force de l'unité centrale\nEtait de rester d'une froide logique.\nMais, voyez-vous, ce petit animal\nAvait un cœur de braise, un grand sens de l'éthique.\n\nElle se sentait déconsidérée devant les raccourcis,\nSoutenant pourtant mon regard cathodique\nSon ventre de rubis glissait, si doux sur le tapis,\nMais le temps entamait sa destruction méthodique.\n\nJ'aurais aimé avoir le temps de te dire tout mon amour\nLorsque ma main te guidait doucement.\nJ'aurais aimé te dire que pour toujours\nJe me souviendrais du galbe de ton dos, tel un amant.\n\nTu es morte aujourd'hui, pendue au bout du tiroir.\nJe regarde gourmand, vers une belle remplaçante.\nNe t'inquiète pas, je n'oublierai jamais les soirs,\nOu de mes doigts, je caressais, léger, mon amante.\n\n\n";
-	char * message_400="\nHTTP/1.1 400 Bad Request\n\rConnection: close\n\rContent-Length: 17 \n\n\r400 Bad request\n\r";
-	char * message_404="\nHTTP/1.1 404 Not Found\n\rConnection: close\n\rContent-Length: 17 \n\n\r404 Not Found\n\r";
-	char * message_ok="\nHTTP/1.1 200 OK\n\rContent-Length: ";
-	int tour = 0;
-	int error400 = 1;
-	int error404 = 0;
+	/*char * message_bienvenue="\nBienvenue sur le serveur de Alexandre Dupriez et Bastien Dusart. Pour vous accueillir dans les meilleures conditions, nous vous offrons ce sublime poème:\n\nMa souris.\nMa souris s'est pendue ce matin.\nJe me doutais bien que quelque chose\nNe tournait pas rond. La mollette sans entrain,\nLe pointeur sans vigueur, tout semblait morose.\n\nEn allumant mon écran j'ai vite reconnu\nSon écriture en Gothic (12pts) en gras.\nC'était son testament, sa lettre de pendue\nQui, le clavier à se consoler, aidera.\n\nJe savais que la force de l'unité centrale\nEtait de rester d'une froide logique.\nMais, voyez-vous, ce petit animal\nAvait un cœur de braise, un grand sens de l'éthique.\n\nElle se sentait déconsidérée devant les raccourcis,\nSoutenant pourtant mon regard cathodique\nSon ventre de rubis glissait, si doux sur le tapis,\nMais le temps entamait sa destruction méthodique.\n\nJ'aurais aimé avoir le temps de te dire tout mon amour\nLorsque ma main te guidait doucement.\nJ'aurais aimé te dire que pour toujours\nJe me souviendrais du galbe de ton dos, tel un amant.\n\nTu es morte aujourd'hui, pendue au bout du tiroir.\nJe regarde gourmand, vers une belle remplaçante.\nNe t'inquiète pas, je n'oublierai jamais les soirs,\nOu de mes doigts, je caressais, léger, mon amante.\n\n\n";
+	*/
 
 		if(fork()==0){
 
-			char buf[512];
-			fclient = fdopen(socket_client, "w+");
-			char * message;
-			while((message=fgets_or_exit(buf, sizeof(buf)/sizeof(buf[0]), fclient)))
-			{
+			    FILE *client = fdopen(socket_client, "w+");	
 
+				char first_line[255];
+				fgets_or_exit(first_line, 255, client);
+				skip_headers(client);
 
-						if(error404==1 && ((strcmp(message,"\r\n")==0)||(strcmp(message,"\n")==0))){
-							envoie_reponse(fclient,message_404);
-							tour=0;
-							error404=0;
-						}
-						else if(error400==1 && ((strcmp(message,"\r\n")==0)||(strcmp(message,"\n")==0))){
-							envoie_reponse(fclient,message_400);
-							tour=0;
-						}
-						else if(error400==0 && ((strcmp(message,"\r\n")==0)||(strcmp(message,"\n")==0))){
-							char str[15];
-							sprintf(str, "%d", (int)strlen(message_bienvenue));
-							char * message_end = concat(str,"\n\n\r200 OK\n\r");
-							message_ok = concat(message_ok,message_end);
-							envoie_reponse(fclient,message_ok);
-							message_ok="\nHTTP/1.1 200 OK\n\rContent-Length: ";
-							error400=1;
-							tour=0;
-							envoie_reponse(fclient,message_bienvenue);
-						}
+				http_request request;
+				int bad_request = parse_http_request(first_line, &request);				
+				
+				if(bad_request==-1)
+					send_response(client, 400, "Bad Request", "Bad request");
+				else if (request.method == HTTP_UNSUPPORTED)
+					send_response(client, 405, "Method Not Allowed", "Method Not Allowed");
+				else if (strcmp(request.url , "/") == 0)
+					send_response(client, 200, "OK", "Bienvenue");
+				else
+					send_response(client, 404, "Not Found", "Not Found");
 
-						if(analyse_entete(message)==0 && tour==0){
-							error400=0;
-						}
-						else if(analyse_entete(message)==404 && tour==0){
-							error404=1;
-						}
+		}
 
-						if(message && ((strcmp(message,"\r\n")!=0)&&(strcmp(message,"\n")!=0))){
-						tour++;
-						}
-			}
+		else{
+			close(socket_client);
 		}
 	}
 
-	//close(socket_client);
-	//close(socket_serveur);
 
 	return socket_serveur;
 }
